49003d2bef7588824925721cd1240b4cc6dcacd6
Jeedug <jedudev1707@gmail.com>
2025-11-20 03:12:10 -0600

    sd

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const historyRoutes = require('./routes/historyRoutes');
 const statisticsRoutes = require('./routes/statisticsRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use(cors());
